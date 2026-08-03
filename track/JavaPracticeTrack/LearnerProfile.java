6605e07620d04d977bcaee858d52bd0609765c56
poludeepthi <deepthiii322@gmail.com>
2026-08-03 21:16:42 +0530

    simplelearnerprofile

---
+++ track/JavaPracticeTrack/LearnerProfile.java
@@ -0,0 +1,14 @@
+
+public class LearnerProfile {
+
+    public static void main(String[] args) {
+        String learnerName = "deepthi";
+        int completedProblems = 30;
+        double practiceHours = 2.0;
+        boolean projectCompleted = true;
+        System.out.println("Learner: " + learnerName);
+        System.out.println("Problems Completed: " + completedProblems);
+        System.out.println("Practice Hours: " + practiceHours);
+        System.out.println("Project Completed: " + projectCompleted);
+    }
+}
