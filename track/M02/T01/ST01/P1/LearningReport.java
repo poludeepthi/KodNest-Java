526b6ff8e53527900e53989e64f06aef1e1b1a4c
poludeepthi <deepthiii322@gmail.com>
2026-08-04 21:34:45 +0530

    learningreport

---
+++ track/M02/T01/ST01/P1/LearningReport.java
@@ -0,0 +1,17 @@
+
+class LearningReport {
+
+    public static void main(String[] args) {
+        int completedTopics = 17;
+        int totalTopics = 20;
+        int dailyLearningHours = 3;
+        int learningDays = 5;
+        int remainingTopics = totalTopics - completedTopics;
+        int weeklyLearningHours = dailyLearningHours * learningDays;
+        double progressPercentage = (double) (completedTopics * 100) / totalTopics;
+        System.out.println("completed Topics: " + completedTopics);
+        System.out.println("RemainingTopics: " + remainingTopics);
+        System.out.println("Weekly Learning Hours: " + weeklyLearningHours);
+        System.out.println("Progress Percentage:" + progressPercentage);
+    }
+}
