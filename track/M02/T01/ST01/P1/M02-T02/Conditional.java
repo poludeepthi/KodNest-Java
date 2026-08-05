c63bfd2a034aaaedce15166e8fda10b1fae89cbf
poludeepthi <deepthiii322@gmail.com>
2026-08-05 12:54:08 +0530

    conditional statement

---
+++ track/M02/T01/ST01/P1/M02-T02/Conditional.java
@@ -0,0 +1,33 @@
+
+public class Conditional {
+
+    public static void main(String[] args) {
+
+        int number = -7;
+        int firstScore = 18;
+        int secondScore = 25;
+
+        // Check number type
+        if (number > 0) {
+            System.out.println("Number type: Positive");
+        } else if (number < 0) {
+            System.out.println("Number type: Negative");
+        } else {
+            System.out.println("Number type: Zero");
+        }
+
+        // Check parity
+        if (number % 2 == 0) {
+            System.out.println("Parity: Even");
+        } else {
+            System.out.println("Parity: Odd");
+        }
+
+        // Find larger score
+        if (firstScore >= secondScore) {
+            System.out.println("Larger score: " + firstScore);
+        } else {
+            System.out.println("Larger score: " + secondScore);
+        }
+    }
+}
