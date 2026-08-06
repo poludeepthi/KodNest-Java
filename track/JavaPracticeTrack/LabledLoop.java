b52817422c26281768e3f6655bb419f8e56ca0ce
poludeepthi <deepthiii322@gmail.com>
2026-08-06 13:01:26 +0530

    labledloop

---
+++ track/JavaPracticeTrack/LabledLoop.java
@@ -0,0 +1,15 @@
+
+class LabledLoop {
+
+    public static void main(String[] args) {
+        outer:
+        for (int i = 1; i < 3; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (i == 2) {
+                    break outer;
+                }
+                System.out.println("i: " + i + "j: " + j);
+            }
+        }
+    }
+}
