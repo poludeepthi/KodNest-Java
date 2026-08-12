4378d487e597f2442b9f79617eddbcd4784cd660
poludeepthi <deepthiii322@gmail.com>
2026-08-12 12:04:41 +0530

    Shadowing

--- track/M02/M02-T02/Placement.java
+++ track/M02/M02-T02/Placement.java
@@ -1,10 +1,12 @@
+
 public class Placement {
+
     public static void main(String[] args) {
         int marks = 68;
         int attendance = 80;
         int practiceDays = 3;
 
-        String result = (marks >= 60 && attendance >= 75) ? "Placement Ready" : "Continue Preparation";
+        String result = (marks >= 60 && attendance >= 75) ? "Placement Ready" : "Continue Practice";
         System.out.println(result);
 
         for (int i = 1; i <= practiceDays; i++) {
