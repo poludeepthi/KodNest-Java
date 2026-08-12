d5600bf3c8c7b31f68c03a9da91ed5e1549c62c5
poludeepthi <deepthiii322@gmail.com>
2026-08-12 13:06:04 +0530

    MethodOverloading

---
+++ track/JavaPracticeTrack/MethodOverloading.java
@@ -0,0 +1,40 @@
+
+class MethodOverloading {
+
+    public static void main(String[] args) {
+        Addition al = new Addition();
+        System.out.println(al.add(10, 20.5));
+
+    }
+}
+
+class Addition {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(int a, double b) {
+        return a + b;
+    }
+
+    double add(double a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(int a, double b, double c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b, double c) {
+        return a + b + c;
+    }
+}
