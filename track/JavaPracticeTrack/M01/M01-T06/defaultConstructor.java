c9f1235ce85fcb01586dfe1fd18f9158030176f4
poludeepthi <deepthiii322@gmail.com>
2026-08-14 22:33:46 +0530

    Default Constructor

---
+++ track/JavaPracticeTrack/M01/M01-T06/defaultConstructor.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+class Employee {
+
+    String name;
+    String role;
+
+    Employee(String name) {
+        this.name = name;
+        role = "Trainee";
+    }
+
+    void displayProfile() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Role: " + role);
+    }
+}
+
+public class defaultConstructor {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        Employee e = new Employee(name);
+        e.displayProfile();
+    }
+}
