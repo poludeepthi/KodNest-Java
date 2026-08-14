220a16847da4e2bd3175e1bfde746e583b0c1ee7
poludeepthi <deepthiii322@gmail.com>
2026-08-14 12:41:52 +0530

    Constructor

---
+++ track/M02/M02-T06/Constructor.java
@@ -0,0 +1,14 @@
+
+class Robot {
+
+    Robot() {
+        System.out.println("Beep beep! Robot reporting for Java duty!");
+    }
+}
+
+public class Constructor {
+
+    public static void main(String[] args) {
+        Robot r = new Robot();
+    }
+}
