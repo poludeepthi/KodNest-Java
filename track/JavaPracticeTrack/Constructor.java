641356f15a6af1272ac80fbefcbe5a926d02621c
poludeepthi <deepthiii322@gmail.com>
2026-08-14 11:47:25 +0530

    with constructor

---
+++ track/JavaPracticeTrack/Constructor.java
@@ -0,0 +1,27 @@
+
+class Student {
+
+    String name;
+    int age;
+    double height;
+
+    Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
+
+class Constructor {
+
+    public static void main(String[] args) {
+        Student s1 = new Student("deepa", 20, 4.6);
+        s1.display();
+    }
+}
