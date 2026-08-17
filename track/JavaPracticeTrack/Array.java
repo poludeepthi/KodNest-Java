0f682426963ccc58f86ceb01a5bf0c356554238e
poludeepthi <deepthiii322@gmail.com>
2026-08-17 12:04:49 +0530

    arrays creation

---
+++ track/JavaPracticeTrack/Array.java
@@ -0,0 +1,17 @@
+
+class Array {
+
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        a[0] = 10;
+        a[1] = 12;
+        a[2] = 13;
+        a[3] = 14;
+        a[4] = 15;
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+    }
+}
