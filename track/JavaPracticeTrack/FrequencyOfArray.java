dbbd05ae224f5fb50e64fc8b66a7b5c7e589b61e
poludeepthi <deepthiii322@gmail.com>
2026-08-18 12:34:15 +0530

    frequency of an array

---
+++ track/JavaPracticeTrack/FrequencyOfArray.java
@@ -0,0 +1,23 @@
+
+class FrequencyOfArray {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 10, 30, 10, 40};
+        Freq fs = new Freq();
+        fs.count(arr, 10);
+    }
+}
+
+class Freq {
+
+    int count = 0;
+
+    void count(int arr[], int key) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] == key) {
+                count++;
+            }
+        }
+        System.out.println(key + " is repeated " + count + "times");
+    }
+}
