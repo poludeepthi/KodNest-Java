d1d08be8c13ef11eb1afb455429a1978bd78a27e
poludeepthi <deepthiii322@gmail.com>
2026-08-31 15:10:31 +0530

    max and min

---
+++ track/M03/M03-T01/MaxMin.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class MaxMin {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a[] = new int[n];
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        int max = a[0];
+        int min = a[0];
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            } else if (a[i] < min) {
+                min = a[i];
+            }
+        }
+
+        System.out.println(min + " " + max);
+        scanner.close();
+    }
+}
